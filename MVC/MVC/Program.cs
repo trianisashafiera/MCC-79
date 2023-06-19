3902ded185830c5bc606525d54aaf5c3c8839b05
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-19 14:01:52 +0700

    Update Program.cs

--- MVC/MVC/Program.cs
+++ MVC/MVC/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DatabaseConnectivity.Controllers;
+
+namespace DatabaseConnectivity
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            new MainMenuController().MainMenu();
+        }
+    }
+}

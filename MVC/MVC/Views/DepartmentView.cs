00bc2752a5953b15d1b6f16c0bf029d4b28083fb
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-19 14:17:04 +0700

    Update DepartmentView.cs

--- MVC/MVC/Views/DepartmentView.cs
+++ MVC/MVC/Views/DepartmentView.cs
@@ -8,8 +8,8 @@ namespace DatabaseConnectivity.View
         {
             foreach (Department department in departments)
             {
-                Console.WriteLine("id : " department.Id);
-                Console.WriteLine("Department Name : " department.Name);
+                Console.WriteLine("id : " + department.Id);
+                Console.WriteLine("Department Name : " + department.Name);
                 Console.WriteLine("Location Id : " + department.locationId);
                 Console.WriteLine("Manager Id : " + department.managerId);
                 Console.WriteLine();

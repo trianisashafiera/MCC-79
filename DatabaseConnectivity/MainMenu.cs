43531630b1fd67627ae73102eeed2ab2af9f6d0f
trianisashafiera <triannisa.shafiera@gmail.com>
2023-06-19 10:02:23 +0700

    Update MainMenu.cs

--- DatabaseConnectivity/MainMenu.cs
+++ DatabaseConnectivity/MainMenu.cs
@@ -8,13 +8,13 @@ namespace DatabaseConnectivity
 {
     class MainMenu
     {
-        protected Region region = new Region();
-        protected Country country = new Country();
-        protected Location location = new Location();
-        protected Employee employee = new Employee();
-        protected Department department = new Department();
-        protected Job job = new Job();
-        protected History history = new History();
+        public Region region = new Region();
+        public Country country = new Country();
+        public Location location = new Location();
+        public Employee employee = new Employee();
+        public Department department = new Department();
+        public Job job = new Job();
+        public History history = new History();
 
         public void Menu()
         {
